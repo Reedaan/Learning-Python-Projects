744a6b6692e03c259c8947678a4004e24340c6b3
Redan <reedaann@gmail.com>
2023-09-27 13:02:30 +0200

    Added saving tasks to a file and organised the code

--- 1. TODO list/main.py
+++ 1. TODO list/main.py
@@ -1,9 +1,20 @@
-task_list = []
+try:
+    data = open("/home/odinproject/repos/Learning-Python-Projects/1. TODO list/tasks.txt", "r").readlines()
+    task_list = [line.strip("\n") for line in data]
+except FileNotFoundError:
+    data = open("/home/odinproject/repos/Learning-Python-Projects/1. TODO list/tasks.txt", "x")
+    task_list = []
+
 exit = False
 
 def view_tasks(task_list):
     for index, task in enumerate(task_list):
         print(f"{index}. {task}")
+
+def save_task(task_list):
+    with open("/home/odinproject/repos/Learning-Python-Projects/1. TODO list/tasks.txt", "w") as f:
+            for line in task_list:
+                f.write(f"{line}\n")
 
 def add_task(task_list):
     task_name = input("What's the task?:")
@@ -12,29 +23,37 @@ def add_task(task_list):
 def remove_task(task_list):
     for index, task in enumerate(task_list):
         print(f"{index}. {task}")
-    task_number = input("Which task would you like to remove? (press enter to cancel)")
+    task_number = input("Which task would you like to remove? (press enter to cancel)\n")
     if task_number == "":
         pass
     else:
         task_list.remove(task_list[int(task_number)])
-    
-while exit == False:
-    print("Hi! Welcome to your todo list. What would you like to do?")
+
+def display():
+    global exit
     user_choice = input('''
-    Press "1" to view your task list.
-    Press "2" to add a new task
-    Press "3" to remove a task.
+    Type "view" to view your task list.
+    Type "add" to add a new task
+    Type "remove" to remove a task.
     Type "exit" to quit the program.
     ''')
     if user_choice.lower() == "exit":
         exit = True 
-    elif user_choice == "1":
+        save_task(task_list)
+    elif user_choice.lower() == "save":
+        save_task(task_list)
+    elif user_choice == "view":
         view_tasks(task_list)
-        input("Press any key to continue...")
-    elif user_choice == "2":
+        input("Press enter to continue...")
+    elif user_choice == "add":
         add_task(task_list)
-    elif user_choice == "3":
+        print("Task has been added!")
+    elif user_choice == "remove":
         remove_task(task_list)
+
+while exit == False:
+    print("Welcome to your task list!")
+    display()
 
 
 
