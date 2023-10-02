997ca7a648140a0cb21a810754926de9264417ed
Redan <reedaann@gmail.com>
2023-10-02 08:47:00 +0200

    Reformatted the code with isort

--- 1. TODO list/main.py
+++ 1. TODO list/main.py
@@ -1,44 +1,59 @@
 try:
-    data = open("/home/odinproject/repos/Learning-Python-Projects/1. TODO list/tasks.txt", "r").readlines()
+    data = open(
+        "/home/odinproject/repos/Learning-Python-Projects/1. TODO list/tasks.txt", "r"
+    ).readlines()
     task_list = [line.strip("\n") for line in data]
 except FileNotFoundError:
-    data = open("/home/odinproject/repos/Learning-Python-Projects/1. TODO list/tasks.txt", "x")
+    data = open(
+        "/home/odinproject/repos/Learning-Python-Projects/1. TODO list/tasks.txt", "x"
+    )
     task_list = []
 
 exit = False
+
 
 def view_tasks(task_list):
     for index, task in enumerate(task_list):
         print(f"{index}. {task}")
 
+
 def save_task(task_list):
-    with open("/home/odinproject/repos/Learning-Python-Projects/1. TODO list/tasks.txt", "w") as f:
-            for line in task_list:
-                f.write(f"{line}\n")
+    with open(
+        "/home/odinproject/repos/Learning-Python-Projects/1. TODO list/tasks.txt", "w"
+    ) as f:
+        for line in task_list:
+            f.write(f"{line}\n")
+
 
 def add_task(task_list):
     task_name = input("What's the task?:")
     task_list.append(task_name)
 
+
 def remove_task(task_list):
     for index, task in enumerate(task_list):
         print(f"{index}. {task}")
-    task_number = input("Which task would you like to remove? (press enter to cancel)\n")
+    task_number = input(
+        "Which task would you like to remove? (press enter to cancel)\n"
+    )
     if task_number == "":
         pass
     else:
         task_list.remove(task_list[int(task_number)])
 
+
 def display():
     global exit
-    user_choice = input('''
+    user_choice = input(
+        """
     Type "view" to view your task list.
     Type "add" to add a new task
     Type "remove" to remove a task.
     Type "exit" to quit the program.
-    ''')
+    """
+    )
     if user_choice.lower() == "exit":
-        exit = True 
+        exit = True
         save_task(task_list)
     elif user_choice.lower() == "save":
         save_task(task_list)
@@ -51,16 +66,7 @@ def display():
     elif user_choice == "remove":
         remove_task(task_list)
 
+
 while exit == False:
     print("Welcome to your task list!")
     display()
-
-
-
-
-
-
-
-
-
-
--- 2. Calculator/main.py
+++ 2. Calculator/main.py
@@ -1,31 +1,71 @@
-import tkinter as tk
 import random
-#TODO getting hold of text from pressed button
-#
+import tkinter as tk
 
 window = tk.Tk()
 window.title("Old runescape bank login")
 
-def get_text():
-    print()
 
-def shuffle(): 
+def shuffle(button):
     random.shuffle(POSITIONS)
     for number, position in enumerate(POSITIONS):
-        button_dict[f"button_{number}"].grid(row=position[0], column=position[1])    
+        button_dict[f"button_{number}"].grid(row=position[0], column=position[1])
+    pressed_list.append(button)
+    pressed_label.config(text=pressed_list)
+    if len(pressed_list) >= 4:
+        pressed_list.clear()
+        pressed_label.config(text="Incorrect password")
 
-pressed_label = tk.Label(text="")
-pressed_label.grid(row=0, columnspan=3)
+
+pressed_label = tk.Label(text="", font=("Arial"))
+pressed_label.grid(row=0, column=0, columnspan=3, padx=10, pady=10, sticky="nsew")
+pressed_list = []
 
 button_dict = {}
 
-#Grid positions row:column
-POSITIONS = [(3,0), (3,1), (3,2), (2,0), (2,1), (2,2), (1,0), (1,1), (1,2), (4,1)]
+# Grid positions row:column
+POSITIONS = [
+    (3, 0),
+    (3, 1),
+    (3, 2),
+    (2, 0),
+    (2, 1),
+    (2, 2),
+    (1, 0),
+    (1, 1),
+    (1, 2),
+    (4, 1),
+]
 
-#Creating a dictionary of button objects, example: button_0 = "0"}
+# Creating a dictionary of button objects, example: button_0 = "0"
 for number in range(10):
-    button = button_dict["button_{0}".format(number)] = tk.Button(text=f"{number}", command=shuffle)
+    button = button_dict["button_{0}".format(number)] = tk.Button(
+        text=f"{number}",
+        height=2,
+        width=2,
+        font=("Arial", 12),
+        command=lambda button=number: shuffle(button),
+    )
 
-shuffle()
+# Starting the program without using the function, so it correctly counts pressed numbers
+random.shuffle(POSITIONS)
+for number, position in enumerate(POSITIONS):
+    button_dict[f"button_{number}"].grid(
+        row=position[0], column=position[1], padx=5, pady=5, sticky="nsew"
+    )
+
+# Make all columns stretch equally
+for i in range(3):
+    window.columnconfigure(i, weight=1)
+
+# Make the window resizable
+window.rowconfigure(0, weight=1)
+
+# Center the window on the screen
+window.update_idletasks()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+x = (screen_width - window.winfo_reqwidth()) // 2
+y = (screen_height - window.winfo_reqheight()) // 2
+window.geometry("+{}+{}".format(x, y))
 
 window.mainloop()
